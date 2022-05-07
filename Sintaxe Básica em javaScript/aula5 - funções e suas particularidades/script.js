4209895a45dfe20fa7a0cedffe3cae0e5f60c56f
br-marcel-oliveira <br.marcel.oliveira@gmail.com>
2022-05-07 12:36:40 -0300

    aula5

---
+++ Sintaxe Básica em javaScript/aula5 - funções e suas particularidades/script.js
@@ -0,0 +1,11 @@
+/* function funcao(){
+    console.log('Essa mensagem veio de uma função');
+}
+
+funcao(); */
+
+function mensagem(primeiro, segundo){
+    console.log(primeiro, segundo);
+}
+
+mensagem('Tudo certo', 'jovem!');
